11eae1d63a49925afcf239a761adcb2fc86b08ad
Christopher Lane <Christopher.Lane@hi-teceurope.com>
2024-11-14 17:20:55 +0000

    Many changes all functionality.
    Style.css stripped out all unecessary styles most of these were then
    added to script.js and applied via the makeGrid function. Including
    flex, flexwrap and width and height of grid.
    Grid now displaying the correct amount of squares and can now be changed
    via user prompt up to 100 by 100 grid.
    Random colours added to cells as well opacity via new function and the
    editing of the makegrid function.
    Functionality complete.

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 
+const reset = document.querySelector("button");
+
+let response = 0; 
+
+reset.addEventListener("click", () => {
+  let response = parseInt(prompt("How many squares you want? "), 10) || 0;
+  if (response > 100){
+    alert('Please try again with a number less than 100');
+  } else {
+  makeGrid(response);
+  }
+});
+
 const container = document.querySelector(".container");
 
-  function makeGrid() {
-    for (let i = 0; i <= 16; i++) {
-      for (let j = 0; j <= 16; j++){
+
+  function makeGrid(response) {
+    container.innerHTML = '';
+    container.style.display = 'flex';
+    container.style.flexWrap = 'wrap';
+    container.style.width = '600px';
+    container.style.height = '600px';
+
+    const cellSize = 600 / response; 
+
+    for (let i = 0; i < response * response; i++) {
+      
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+
+        cell.addEventListener("mouseenter", ()=> setBg(cell));
+        cell.addEventListener("mouseover", (event) => { 
+          let currentOpacity = parseFloat(event.target.style.opacity) || 1;
+          if (currentOpacity > 0){
+            event.target.style.opacity = (currentOpacity - 0.1).toFixed(2);
+          }
+
+        });
+
         container.appendChild(cell);
+        console.log(response);
       }
         
-      }
-    }
-
-  makeGrid();
- const cellHover = document.querySelectorAll(".cell");
+      };
+    
+ const setBg = (cell) => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  cell.style.backgroundColor = "#" + randomColor;
+  
+ };
  
 
- cellHover.forEach((cell) => {
-    cell.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "red";
-    });
- });
+ 
